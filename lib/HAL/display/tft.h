5727ad55abadb64a0e9218d3b13a0352639c030a
cruddierosprey <99515038+cruddierosprey@users.noreply.github.com>
2026-09-03 14:54:38 +0300

    feat(display): route SSD1306 builds to Adafruit backend

--- lib/HAL/display/tft.h
+++ lib/HAL/display/tft.h
@@ -2,7 +2,8 @@
 #define LIB_HAL_DISPLAY_TFT_H
 #include <pins_arduino.h>
 
-#if !defined(USE_ARDUINO_GFX) && !defined(USE_LOVYANGFX) && !defined(USE_TFT_ESPI) && !defined(USE_M5GFX)
+#if !defined(USE_ADAFRUIT_SSD1306) && !defined(USE_ARDUINO_GFX) && !defined(USE_LOVYANGFX) && \
+    !defined(USE_TFT_ESPI) && !defined(USE_M5GFX)
 #define USE_TFT_ESPI
 #endif
 
@@ -10,7 +11,11 @@ class TFT_eSPI;
 class tft_sprite;
 class tft_logger;
 #include <SPI.h>
-#if defined(USE_TFT_ESPI)
+
+#if defined(USE_ADAFRUIT_SSD1306)
+#include "adafruit_ssd1306.h"
+
+#elif defined(USE_TFT_ESPI)
 #include "tftespi.h"
 
 #elif defined(USE_ARDUINO_GFX)
